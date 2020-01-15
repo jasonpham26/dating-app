e47218cef157496e0eb07cfc3de02d6c6ffaffe8
jason <vinhphuong0206@gmail.com>
2020-01-15 18:49:06 +1000

    changed AuthController

--- Dating.API/Controllers/AuthController.cs
+++ Dating.API/Controllers/AuthController.cs
@@ -6,34 +6,35 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace Dating.API.Controllers
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class AuthController : ControllerBase
+  [Route("api/[controller]")]
+  [ApiController]
+  public class AuthController : ControllerBase
+  {
+    private readonly IAuthRepository _repo;
+
+    public AuthController(IAuthRepository repo)
     {
-        private readonly IAuthRepository _repo;
-
-        public AuthController(IAuthRepository repo)
-        {
-            _repo = repo;
-        }
-
-        [HttpPost("register")]
-        public async Task<IActionResult> Register(UserForRegisterDto userForRegisterDto)
-        {
-            userForRegisterDto.Username = userForRegisterDto.Username.ToLower();
-
-            if(await _repo.UserExist(userForRegisterDto.Username))
-                return BadRequest("Username already exist");
-
-            var userToCreate = new User
-            {
-                Username = userForRegisterDto.Username
-            };
-
-            var createdUser = await _repo.Register(userToCreate, userForRegisterDto.Password);
-
-            return StatusCode(201);
-        }
-
+      _repo = repo;
     }
+
+    [HttpPost("register")]
+    public async Task<IActionResult> Register(UserForRegisterDto userForRegisterDto)
+    {
+
+      userForRegisterDto.Username = userForRegisterDto.Username.ToLower();
+
+      if (await _repo.UserExist(userForRegisterDto.Username))
+        return BadRequest("Username already exist");
+
+      var userToCreate = new User
+      {
+        Username = userForRegisterDto.Username
+      };
+
+      var createdUser = await _repo.Register(userToCreate, userForRegisterDto.Password);
+
+      return StatusCode(201);
+    }
+
+  }
 }
